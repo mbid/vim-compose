baae52dc2df2314197498c3a278351f6893b2764
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-12-12 18:43:02 +0100

    Migrate background.ts and add formatter

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,15 +1,35 @@
-let active = false;
+import Port = chrome.runtime.Port;
+import Tab = chrome.tabs.Tab;
 
-function makeOrange(color: string): void {
-    document.body.style.backgroundColor = color;
+chrome.action.onClicked.addListener((tab: Tab) => {
+  if (tab.id == null) {
+    return;
+  }
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"],
+  });
+});
+
+function connectPorts(lhs: Port, rhs: Port) {
+  function oneWay(from: Port, to: Port) {
+    from.onMessage.addListener((message) => {
+      to.postMessage(message);
+    });
+    from.onDisconnect.addListener(() => {
+      to.disconnect();
+    });
+  }
+
+  oneWay(lhs, rhs);
+  oneWay(rhs, lhs);
 }
 
-chrome.action.onClicked.addListener((tab) => {
-    active = !active;
-    const color = active ? 'orange' : 'white';
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id ? tab.id : -1},
-        func: makeOrange,
-        args: [color]
-    }).then();
+chrome.runtime.onConnect.addListener((contentPort: Port) => {
+  console.assert(contentPort.name === "content");
+
+  const nativeHost = "com.mbid.vim.compose";
+  const nativePort = chrome.runtime.connectNative(nativeHost);
+
+  connectPorts(contentPort, nativePort);
 });
